905eb04345300fa013f20320ab480ccc6fc0e15a
Konstantin S. Ivanov <ksiv@yandex.ru>
2020-10-30 01:50:24 +0300

    prototype native added

--- README.md
+++ README.md
@@ -18,7 +18,8 @@ Examples are in form of Junit 5 but they serve as usage examples not as tests.
 10. Chain of Responsibility
 11. Decorator  
 12. Template  
-13. Composite
+13. Composite  
+14. Prototype  
 
 # gist
 The idea of these samples to provide an easy to understand example.
--- src/test/java/Patterns/Prototype/nativeClonableItemTest.java
+++ src/test/java/Patterns/Prototype/nativeClonableItemTest.java
@@ -3,17 +3,17 @@ package Patterns.Prototype;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class nativeClonableItemTest {
 
     @Test
     void testClone() throws CloneNotSupportedException {
         icecream icecreamChocolateTop = new icecream();
         icecreamChocolateTop.setTopping(new topping("chocolate"));
+        icecream icecreamChocolaTopSecond = (icecream)icecreamChocolateTop.clone();
         icecream icecreamWaffleTop = (icecream) icecreamChocolateTop.clone();
         icecreamWaffleTop.setTopping(new topping("Waffle"));
         Assertions.assertEquals(icecreamChocolateTop.topping.getToppingName(),"chocolate");
+        Assertions.assertEquals(icecreamChocolaTopSecond.topping.getToppingName(),"chocolate");
         Assertions.assertEquals(icecreamWaffleTop.topping.getToppingName(),"Waffle");
         // native equals&hashcode
         Assertions.assertNotEquals(icecreamChocolateTop.hashCode(),icecreamWaffleTop.hashCode());
