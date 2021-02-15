dfcbad13f37e0bf6257d911c29b4f2232249dae4
Konstantin S. Ivanov <ksiv@yandex.ru>
2021-02-15 22:51:53 +0300

    typo

--- src/test/java/Patterns/State/ContextRoomLightTest.java
+++ src/test/java/Patterns/State/ContextRoomLightTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class ContextRoomLightTest {
 
     /*
-    *   Here a room light with pull cord switch. One pull the cord to turn light on \off
+    *   Here a room light with pull cord switch. One pulls the cord to turn light on \off
     *   These are common on light sources, and ceiling fans.
     *   One method "pullTheCord" that does an action depending on current state.
     *
